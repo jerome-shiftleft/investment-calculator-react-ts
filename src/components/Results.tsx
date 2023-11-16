1f63dc7bcc49e410bdd0596d057a2053bb0b399c
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-17 07:09:36 +0800

    progress

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,5 +1,5 @@
 import type { UserInputType } from "../types";
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 type Props = {
   input: UserInputType;
@@ -12,7 +12,28 @@ function Results({ input }: Props) {
   console.log(resultsData);
   return (
     <>
-      <p>Results...</p>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          {resultsData.map(yearData => (            
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td></td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
