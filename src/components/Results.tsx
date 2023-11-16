e4b7189034075a5df7e7f63ca5dfeda19dbb2f17
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-17 06:58:07 +0800

    progress

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,11 +1,15 @@
 import type { UserInputType } from "../types";
+import { calculateInvestmentResults } from "../util/investment";
 
 type Props = {
   input: UserInputType;
 };
 
 function Results({ input }: Props) {
-  console.log(input);
+  //console.log(input);
+  const resultsData = calculateInvestmentResults(input);
+  console.log('results data:');
+  console.log(resultsData);
   return (
     <>
       <p>Results...</p>
